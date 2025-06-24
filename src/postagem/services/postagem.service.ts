21a175c042d2f787af5c41ca44e63e6880b30d16
Natalia Taira <nataliagtaira@gmail.com>
2025-06-24 13:54:13 -0300

    Adiciona método update, delete, findEntreDatas, findByTitulo, findByID e create

--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -1,16 +1,77 @@
 import { InjectRepository } from "@nestjs/typeorm";
 import { Postagem } from "../entities/postagem.entity";
-import { Repository } from "typeorm";
-import { Injectable } from "@nestjs/common";
+import { Between, DeleteResult, Repository } from "typeorm";
+import { BadRequestException, HttpException, HttpStatus, Injectable, Param, ParseIntPipe } from "@nestjs/common";
 
 @Injectable()
 export class PostagemService {
+
     constructor(
         @InjectRepository(Postagem)
         private PostagemRepository: Repository<Postagem>
     ) { }
 
+    async create(postagem: Postagem): Promise<Postagem> {
+        return await this.PostagemRepository.save(postagem);
+    }
+
+    async findByID(id: number): Promise<Postagem> {
+        const postagem = await this.PostagemRepository.findOne({
+            where: {
+                id
+            }
+        });
+
+        if (!postagem) {
+            throw new HttpException('Postagem não encontrada!', HttpStatus.NOT_FOUND);
+        }
+
+        return postagem;
+    }
+
+    async findByTitulo(titulo: string): Promise<Postagem> {
+        const postagem = await this.PostagemRepository.findOne({
+            where: {
+                titulo
+            }
+        });
+
+        if (!postagem) {
+            throw new HttpException('Postagem não encontrada', HttpStatus.NOT_FOUND);
+        }
+
+        return postagem;
+    }
+
+    async findEntreDatas(dataInicial: Date, dataFinal: Date): Promise<Postagem[]> {
+        if (dataInicial > dataFinal) {
+            throw new BadRequestException('Data inicial não pode ser maior que a data final');
+        }
+        
+        const postagem = await this.PostagemRepository.find({
+            where: {
+                data: Between(dataInicial, dataFinal)
+            }
+        });
+
+        if (!postagem) {
+            throw new HttpException('Postagem não encontrada', HttpStatus.NOT_FOUND);
+        }
+
+        return postagem;
+    }
+
     async findAll(): Promise<Postagem[]> {
         return await this.PostagemRepository.find();
+    }
+
+    async update(postagem: Postagem):Promise<Postagem> {
+        await this.findByID(postagem.id);
+        return this.PostagemRepository.save(postagem);
+    }
+
+    async delete(id: number): Promise<DeleteResult> {
+        await this.findByID(id);
+        return await this.PostagemRepository.delete(id);
     }
 }
