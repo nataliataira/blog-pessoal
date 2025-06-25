9f3f282a65d1b5fb79f3666c2e993765dcd2f97e
Natalia Taira <nataliagtaira@gmail.com>
2025-06-25 17:26:13 -0300

    Insere a relação entre postagem e tema

--- src/tema/entities/tema.entity.ts
+++ src/tema/entities/tema.entity.ts
@@ -1,7 +1,8 @@
 import { IsNotEmpty } from "class-validator";
-import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+import { Postagem } from "src/postagem/entities/postagem.entity";
+import { Column, Entity, OneToMany, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
 
-@Entity({name: "tb_tema"})
+@Entity({name: "tb_temas"})
 export class Tema {
     @PrimaryGeneratedColumn()
     id: number;
@@ -9,4 +10,7 @@ export class Tema {
     @IsNotEmpty()
     @Column({length: 255, nullable: false})
     descricao: string;
+
+    @OneToMany(() => Postagem, (postagem => postagem.tema))
+    postagem: Postagem[];
 }
