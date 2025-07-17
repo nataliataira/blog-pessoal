fc9e749a1bfa42aaab9d288e26a094fe75aa4188
Natalia Taira <nataliagtaira@gmail.com>
2025-07-16 22:21:55 -0300

    Deploy do Projeto Blog Pessoal

--- src/usuario/entities/usuario.entity.ts
+++ src/usuario/entities/usuario.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger"
 import { IsNotEmpty, IsEmail, MinLength } from "class-validator"
 import { Postagem } from "src/postagem/entities/postagem.entity"
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm"
@@ -5,26 +6,32 @@ import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm"
 @Entity({name: "tb_usuarios"})
 export class Usuario {
 
-    @PrimaryGeneratedColumn() 
+    @PrimaryGeneratedColumn()
+    @ApiProperty()
     id: number
 
     @IsNotEmpty()
-    @Column({length: 255, nullable: false}) 
+    @Column({length: 255, nullable: false})
+    @ApiProperty()
     nome: string
 
     @IsEmail()
     @IsNotEmpty()
-    @Column({length: 255, nullable: false })
+    @Column({length: 255, nullable: false})
+    @ApiProperty({example: "email@email.com.br"})
     usuario: string
 
     @MinLength(8)
     @IsNotEmpty()
-    @Column({length: 255, nullable: false }) 
+    @Column({length: 255, nullable: false})
+    @ApiProperty()
     senha: string
 
-    @Column({length: 5000 }) 
+    @ApiProperty()
+    @Column({length: 5000}) 
     foto: string
 
+    @ApiProperty()
     @OneToMany(() => Postagem, (postagem) => postagem.usuario)
     postagem: Postagem[]
 
